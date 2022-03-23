170de913f03e0b96fa60d45380d3439fbd16100f
zagi <biuro@zagalski.pl>
2022-03-23 18:24:43 +0100

    Add Vuetify plugin and import it

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
+import vuetify from '@/plugins/veutfiy';
+
+
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 
 new Vue({
+  vuetify,
   router,
   render: (h) => h(App),
 }).$mount('#app');
